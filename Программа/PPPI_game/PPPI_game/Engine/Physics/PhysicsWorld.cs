df772ca1e712d098d1dcbb14972fc0393b5d8c1d
Danilevich Vladislav <v.danilevitch2013@yandex.ru>
2023-03-28 18:34:42 +0300

    Добавил структуру Object для Physics

---
+++ Программа/PPPI_game/PPPI_game/Engine/Physics/PhysicsWorld.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPPI_game.Engine.Physics
+{
+    // Информация об объекте
+    struct Object
+    {
+
+    }
+
+    internal class PhysicsWorld
+    {
+
+    }
+}
