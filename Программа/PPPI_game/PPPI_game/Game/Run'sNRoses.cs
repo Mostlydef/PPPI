afc66ddcb3f155e9d6dafb13593ff8087bc21fa7
Danilevich Vladislav <v.danilevitch2013@yandex.ru>
2023-03-28 20:42:26 +0300

    Добавил конструктор класса Run_sNRoses

---
+++ Программа/PPPI_game/PPPI_game/Game/Run'sNRoses.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPPI_game.Game
+{
+    internal abstract class Run_sNRoses
+    {
+        private int _damage;
+        private int _rate;
+        private int _distance;
+        private int _magazine;
+
+        public Run_sNRoses(int damage, int rate, int distance, int magazine)
+        {
+            _damage = damage;
+            _rate = rate;
+            _distance = distance;
+            _magazine = magazine;
+        }
+    }
+}
