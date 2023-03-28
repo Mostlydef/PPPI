d2ccaa0f0fbdfeac9217b364a0d616e716f0b107
Danilevich Vladislav <v.danilevitch2013@yandex.ru>
2023-03-28 18:09:16 +0300

    Добавил метод Transforms для Graphics

---
+++ Программа/PPPI_game/PPPI_game/Engine/Graphics/Multitexturing.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPPI_game.Engine.Graphics
+{
+    internal class Multitexturing
+    {
+
+        // Операции изменения позиции, размера, или ориентации объекта в пространстве.
+        public void Transforms()
+        {
+
+        }
+    }
+}
