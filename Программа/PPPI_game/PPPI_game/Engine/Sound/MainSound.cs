6b27b4b319cd6902bd3e883e00efb2a6afdb1c5e
Danilevich Vladislav <v.danilevitch2013@yandex.ru>
2023-03-28 21:28:59 +0300

    Добавил метод PlayMusic для MainSound

---
+++ Программа/PPPI_game/PPPI_game/Engine/Sound/MainSound.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public enum Groups
+{
+    AC_DC,
+    Pink_Floyd,
+    Aerosmith
+}
+
+namespace PPPI_game.Engine.Sound
+{
+
+    internal class MainSound
+    {
+        Groups _groups; 
+        // Проиграть музыку
+        public void PlayMusic()
+        {
+            _groups = Groups.Aerosmith;
+        }
+    }
+}
